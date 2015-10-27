560ae9f85fe550e5a85f81af319a5dcb670b11c4
Shay <hallshay@gmail.com>
2015-10-27 18:36:47 -0400

    Wire user.html to index.html and to live API

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,21 @@
        .when('/login',{
          templateUrl: 'partials/login.html',
        })
+       .when('/login',{ //CHANGE THIS SO IT REDIRECTS AT THE RIGHT TIME (TO LOGIN)
+         templateUrl: 'partials/user.html',
+       })
+      })
+      .run(function($http, $rootScope) {
+        $http.get('https://lit-hollows-3591.herokuapp.com/api/activities/?format=json')
+        // $http.get('/src/fake.json')
+          .then(function(response) {
+            $rootScope.activities = response.data;
+        })
 
 }) //END module
+
+
+
+
 
 })(); //END OF IIFE
