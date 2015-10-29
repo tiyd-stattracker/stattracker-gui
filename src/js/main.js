dfaa3c1a967d1b06d6f69adccf54fc276ab05ec0
Shay <hallshay@gmail.com>
2015-10-29 07:51:21 -0400

    Wire things together manually for demo and fix minor errors.

--- src/js/main.js
+++ src/js/main.js
@@ -14,24 +14,25 @@
        .when('/activity/:id',{
          templateUrl: 'partials/activity.html',
          controller: function($routeParams, $http, $scope){
-         var id = $routeParams.id
+         var id = $routeParams.id;
            $http.get("https://lit-hollows-3591.herokuapp.com/api/activities/"+ id +"/?format=json")
            .then(function(response){
              $scope.activity = response.data;
-             console.log('here')
-             console.log(response.data.logs[0].activity_id);
-           })
+          // $scope.log = {
+          //   activity_count: '',
+          // }
+          // $scope.submit = function() {
+          //   $http.patch('https://lit-hollows-3591.herokuapp.com/api/activities/1/logs/1?format=json', $scope.logs)
+          //   $scope.logs = {};
+          //   };
+          })
          }
        })
-
-       //.controller('ActivityController', function($scope, $http){
-
-  }) //END $routeProvider    //END module
+    }) //END $routeProvider    //END module
       .controller('ListOfActivitesController', function($scope, $http) {
         $http.get('https://lit-hollows-3591.herokuapp.com/api/activities/?format=json')
           .then(function(response) {
             $scope.activities = response.data;
-            console.log(response.data);
         })
       }) //END of 'ListOfActivitesController'
       .controller("NewActivityController", function($scope, $http) {
@@ -41,11 +42,12 @@
         }
         $scope.submit= function() {
           $http.post('https://lit-hollows-3591.herokuapp.com/api/activities/?format=json', $scope.activity);
+        $scope.activity = {  };
         };
-        $scope.activity = {
-          activity_name: '',
-          start_date: ''
-        };
+        $http.get('https://lit-hollows-3591.herokuapp.com/api/activities/?format=json')
+          .then(function(response) {
+            $scope.activities = response.data;
+        })
       })//END of "NewActivityController"
 
       // .controller('ActivityController', function($scope, $http){
@@ -66,18 +68,16 @@
             $http.post('https://lit-hollows-3591.herokuapp.com/api/users/?format=json', $scope.users)
               $scope.users = {};
           };
-        });//CONTROLLER FOR SIGNUP
+        })//CONTROLLER FOR SIGNUP
       })(); //END OF IIFE
 
 ;(function(){
   $("a.log-info").on('click', function(event){
     event.preventDefault();
-    console.log(this.hash);
     $(this.hash).removeClass("active").siblings().addClass("active")
   });
   $("a.update-info").on('click', function(event){
     event.preventDefault();
-    console.log(this.hash);
     $(this.hash).removeClass("active").siblings().addClass("active")
   });
 
